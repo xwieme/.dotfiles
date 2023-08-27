2a2ddec4904ffd8ae4dfd2365f732d7644005e99
xwieme <xander.wieme@ugent.be>
2023-08-27 18:46:51 +0200

    Add swalock and spaceship config files

---
+++ .config/spaceship/spaceship.zsh
@@ -0,0 +1,21 @@
+SPACESHIP_USER_SHOW=always
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "
+
+SPACESHIP_PROMPT_ORDER=(
+    user
+    dir
+    host
+    git
+    python
+    lua
+    julia
+    docker
+    conda
+    exec_time
+    line_sep
+    jobs
+    exit_code
+    sudo
+    char
+)
