a7f148f6fd58314e166caf404a4ab213b7a308b0
xwieme <xander.wieme@ugent.be>
2025-09-04 17:06:32 +0200

    Update neovim latex/markdown: edit snippets, add compile options to vimtex, add markdown plugin

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -1,8 +1,13 @@
 return {
-  "lervag/vimtex",
-  lazy = false,
-  config = function()
-    vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
-    vim.g.vimtex_view_method = "zathura"
-  end,
+    "lervag/vimtex",
+    lazy = false,
+    config = function()
+        vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
+        vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_compiler_latexmk = {
+            options = {
+                '-shell-escape'
+            }
+        }
+    end,
 }
