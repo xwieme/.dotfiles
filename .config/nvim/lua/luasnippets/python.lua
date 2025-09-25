d5e2f0bced2687f157d929f56a004594c9a2d483
xwieme <xander.wieme@ugent.be>
2025-09-25 09:20:58 +0200

    Edit luasnippets: python and latex environments

--- .config/nvim/lua/luasnippets/python.lua
+++ .config/nvim/lua/luasnippets/python.lua
@@ -3,58 +3,37 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 local fmta = require("luasnip.extras.fmt").fmta
+local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
-ls.add_snippets("python", {
-    s({ trig = "([^%w])mn", snippetType = "autosnippet", regTrig = true, wordTrig = false }, {
-        t('if __name__ == "__main__":'),
-    }),
+return {
+  s({ trig = "([^%w])mn", snippetType = "autosnippet", regTrig = true, wordTrig = false }, {
+    t('if __name__ == "__main__":'),
+  }),
 
-    s(
-        {
-            trig = "([^%w])cc",
-            snippetType = "autosnippet",
-            regTrig = true,
-            wordTrig = false,
-            description = "Insert block comment",
-        },
-        fmta(
-            [[
+  s(
+    { trig = "cc", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         """
         <>
         """
       ]],
-            { i(0) }
-        )
-    ),
+      { i(0) }
+    )
+  ),
 
-    s(
-        {
-            trig = "([^%w])ds",
-            snippetType = "autosnippet",
-            regTrig = true,
-            wordTrig = false,
-            description = "Insert a function docstring",
-        },
-        fmta(
-            [[
+  s(
+    { trig = "ds", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         """
         Args:
             <>
         """
       ]],
-            { i(0) }
-        )
-    ),
-
-    s(
-        {
-            trig = "([^%w])pp",
-            snippetType = "autosnippet",
-            regTrig = true,
-            wordTrig = false,
-            description = "Print a variable",
-        },
-        fmta([[print(f"{ <> = }")]], { i(0) })
+      { i(0) }
     )
+  ),
 
-})
+  s({ trig = "pp", snippetType = "autosnippet", condition = line_begin }, fmta([[print(f"{ <> = }")]], { i(0) })),
+}
--- .config/nvim/lua/luasnippets/tex/environments.lua
+++ .config/nvim/lua/luasnippets/tex/environments.lua
@@ -7,207 +7,219 @@ local rep = require("luasnip.extras").rep
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
 local in_mathzone = function()
-    return vim.fn["vimtex#syntax#in_mathzone"]() == 1
+  return vim.fn["vimtex#syntax#in_mathzone"]() == 1
 end
 
 return {
 
-    s(
-        { trig = "nn", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "nn", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \begin{<>}
             <>
         \end{<>}
         ]],
-            { i(1), i(2), rep(1) }
-        )
-    ),
+      { i(1), i(2), rep(1) }
+    )
+  ),
 
-    s(
-        { trig = "eq", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "eq", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \begin{equation}
             <>
         \end{equation}
         ]],
-            { i(1) }
-        )
-    ),
+      { i(1) }
+    )
+  ),
 
-    s(
-        { trig = "al", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "al", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \begin{align}
             <>
         \end{align}
         ]],
-            { i(1) }
-        )
-    ),
+      { i(1) }
+    )
+  ),
 
-    s(
-        { trig = "it", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "it", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \begin{itemize}
             <>
         \end{itemize}
         ]],
-            { i(1) }
-        )
-    ),
+      { i(1) }
+    )
+  ),
 
-    s(
-        { trig = "en", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "en", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \begin{enumerate}
             <>
         \end{enumerate}
         ]],
-            { i(1) }
-        )
-    ),
+      { i(1) }
+    )
+  ),
 
-    s(
-        { trig = "qu", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "qu", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \begin{quote}
             <>
         \end{quote}
         ]],
-            { i(1) }
-        )
-    ),
+      { i(1) }
+    )
+  ),
 
-    s(
-        { trig = "pm", snippetType = "autosnippet", condition = in_mathzone },
-        fmta(
-            [[
+  s(
+    { trig = "pm", snippetType = "autosnippet", condition = in_mathzone },
+    fmta(
+      [[
         \begin{pmatrix}
             <>
         \end{pmatrix}
     ]],
-            { i(1) }
-        )
-    ),
+      { i(1) }
+    )
+  ),
 
-    s(
-        { trig = "mint", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "mint", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \begin{minted}{ <> }
             <>
         \end{pmatrix}
     ]],
-            { i(1), i(2) }
-        )
-    ),
+      { i(1), i(2) }
+    )
+  ),
 
-    s(
-        { trig = "nt", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "nt", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \NewTcbTheorem[number within=chapter]{theorem}{Theorem}%
             {colback=green!5, colframe=green!35!black,fonttitle=\bfseries}{th}
     ]],
-            {}
-        )
-    ),
+      {}
+    )
+  ),
 
-    s(
-        { trig = "nd", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "nd", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \NewTcbTheorem[number within=chapter]{definition}{Definition}%
             {colback=blue!5, colframe=blue!35!black,fonttitle=\bfseries}{def}
     ]],
-            {}
-        )
-    ),
+      {}
+    )
+  ),
 
-    s(
-        { trig = "nl", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "nl", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \NewTcbTheorem[number within=chapter]{lemma}{Lemma}%
             {colback=red!5, colframe=red!35!black,fonttitle=\bfseries}{lem}
     ]],
-            {}
-        )
-    ),
+      {}
+    )
+  ),
 
-    s(
-        { trig = "np", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "np", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \NewTcbTheorem[number within=chapter]{proposition}{Proposition}%
             {colback=red!5, colframe=red!35!black,fonttitle=\bfseries}{prop}
     ]],
-            {}
-        )
-    ),
+      {}
+    )
+  ),
 
-    s(
-        { trig = "nc", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
+  s(
+    { trig = "nc", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
         \NewTcbTheorem[number within=chapter]{corollary}{Corollary}%
             {colback=red!5, colframe=red!35!black,fonttitle=\bfseries}{col}
     ]],
-            {}
-        )
-    ),
+      {}
+    )
+  ),
 
-    s(
-        { trig = "tb", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
-        \begin{theorem}{ <> }
+  s(
+    { trig = "tb", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
+        \begin{theorem}{ <> }{ <> }
             <>
         \end{theorem}
     ]],
-            { i(1), i(2) }
-        )
-    ),
+      { i(1), i(2), i(3) }
+    )
+  ),
 
-    s(
-        { trig = "db", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
-        \begin{definition}{ <> }
+  s(
+    { trig = "db", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
+        \begin{definition}{ <> }{ <> }
             <>
         \end{definition}
     ]],
-            { i(1), i(2) }
-        )
-    ),
+      { i(1), i(2), i(3) }
+    )
+  ),
 
-    s(
-        { trig = "pb", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
-        \begin{proposition}{ <> }
+  s(
+    { trig = "pb", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
+        \begin{proposition}{ <> }{ <> }
             <>
         \end{proposition}
     ]],
-            { i(1), i(2) }
-        )
-    ),
+      { i(1), i(2), i(3) }
+    )
+  ),
 
-    s(
-        { trig = "cb", snippetType = "autosnippet", condition = line_begin },
-        fmta(
-            [[
-        \begin{corollary}{ <> }
+  s(
+    { trig = "cb", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
+        \begin{corollary}{ <> }{ <> }
             <>
         \end{corollary}
     ]],
-            { i(1), i(2) }
-        )
-    ),
+      { i(1), i(2), i(3) }
+    )
+  ),
+
+  s(
+    { trig = "lb", snippetType = "autosnippet", condition = line_begin },
+    fmta(
+      [[
+        \begin{lemma}{ <> }{ <> }
+            <>
+        \end{lemma}
+    ]],
+      { i(1), i(2), i(3) }
+    )
+  ),
 }
